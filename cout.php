855ca39e2c4303aaaa3deb2690d529f697a6562f
AnnKodynska <kotka219@gmail.com>
2019-11-20 10:05:20 +0200

    fix existing file

--- cout.php
+++ cout.php
@@ -6,37 +6,25 @@
  * Time: 17:05
  */
 
-
-
-//The total of little rectangles is 3x3 grid
-// 1x1 = 1
-// 2x2 =4
-// 3x3 = 9
-//
-//1 little rectangles height x 2 little rectangles length (in one row=2, in 3-columns)= 2x3 = 6
-//
-//1 little rectangles x 3 little rectangles length = 1x3 = 3
-//
-//2 little rectangles height x 1 little rectangles length = 2x3 = 6
-//
-//2 little rectangles height x 3 little rectangles length = 1x2 = 2
-//
-//3 little rectangles height x 1 little rectangles length = 1 x 3 = 3
-//
-//3 little rectangles height x 2 units little rectangles = 1x2 = 2
-//
-//Total 36
-
-function count1($a, $b )  {
+//You can now set the number of dots (not squares)
+function count1($a, $b)
+{
     $sum = 0;
-    for( $i=0; $i<$a; $i++){
-        for( $j=0; $j<$b; $j++){
-            // here I get these combinations and sum them up
-            $sum += ($i+1)*($j+1);
+    for ($i = 1; $i <= $a - 1; $i++) {
+        for ($j = 1; $j <= $b - 1; $j++) {
+            $sum += ($a - $i) * ($b - $j);
         }
     }
     return $sum;
 }
 
+count1(3, 3);
 
-count1(3,3);
+// or using the formula
+function count2($a, $b)
+{
+    $sum = ($a) * ($a - 1) * $b * ($b - 1) / 4;
+    return $sum;
+}
+
+count2(3, 3);
